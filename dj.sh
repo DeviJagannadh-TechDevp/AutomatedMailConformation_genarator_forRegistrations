5604f5d12c5fb45a662498ba6b773c77b2be46cd
Devi Jagannadh Kotha <72683351+DeviJagannadh-TechDevp@users.noreply.github.com>
2020-10-13 14:58:59 +0530

    Update dj.sh

--- dj.sh
+++ dj.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "EMAIL"
+
+
+cat doNotDeleteThis.txt 
+echo " "
+echo "Welcome to Reg Portal"
+echo " "
+
+echo "EMAIL (Mail will be send to this mail)"
 read email
 echo "NAME"
 read name
@@ -19,8 +26,9 @@ read yn
 VAR="y"
 
 tocken=($(shuf -i 1000000000-9999999999 -n 1))
-
-
+echo " "
+echo "Sending Conformation Mail To $email "
+echo " "
 if [ "$yn" = "$VAR" ];
 then
 
@@ -30,9 +38,39 @@ then
 
     echo -e "Subject: Successfully Registered!! \nDear $name ,\n\nYou are successfully registred with ID $id1\n\nYour Unique Token Id is $tocken (Token is very important when entering in to contest ! )\n\nFeel free to contact us if you have any problem ! \n\n\nWith Regards\nDevi Jagannadh Kotha\nTechDevp\ntechdevp.online" > $id1.txt
 
-cat $id1.txt | ssmtp $email
+    cat $id1.txt | ssmtp $email
 
+    echo "Two extra files i.e $id1.txt and universal.txt are created for backup in your local machine would you like to delete it ?? y/n (Small letters)"
 
+    
+     read dj 
+     if [ "$dj" = "y" ];
+     then 
+         sudo rm -f $id1.txt 
+         sudo rm -f universal.txt
+         
+     fi
 
-    echo "File Created ! and Email Sent ! "
+echo "You are done !"
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
